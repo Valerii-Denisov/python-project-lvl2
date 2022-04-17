fbcee179cdfaa353e58ee491600430158a19173e
Valerii-Denisov <klerk919191@gmail.com>
2022-04-17 22:15:03 +0300

    add yaml test

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -10,6 +10,10 @@ PATH_FILE1_YML = 'tests/fixtures/file1.yml'
 PATH_FILE2_YML = 'tests/fixtures/file2.yml'
 PATH_FILE1_NEST_JSON = 'tests/fixtures/file1_nest.json'
 PATH_FILE2_NEST_JSON = 'tests/fixtures/file2_nest.json'
+PATH_FILE1_NEST_YAML = 'tests/fixtures/file1_nest.yaml'
+PATH_FILE2_NEST_YAML = 'tests/fixtures/file2_nest.yaml'
+PATH_FILE1_NEST_YML = 'tests/fixtures/file1_nest.yml'
+PATH_FILE2_NEST_YML = 'tests/fixtures/file2_nest.yml'
 PATH_RESULT = 'tests/fixtures/result'
 PATH_RESULT_NEST = 'tests/fixtures/result_nest'
 
@@ -23,7 +27,9 @@ def get_answer(path):
     (PATH_FILE1_JSON, PATH_FILE2_JSON, PATH_RESULT),
     (PATH_FILE1_YML, PATH_FILE2_YML, PATH_RESULT),
     (PATH_FILE1_YAML, PATH_FILE2_YAML, PATH_RESULT),
-    (PATH_FILE1_NEST_JSON, PATH_FILE2_NEST_JSON, PATH_RESULT_NEST)
+    (PATH_FILE1_NEST_JSON, PATH_FILE2_NEST_JSON, PATH_RESULT_NEST),
+    (PATH_FILE1_NEST_YML, PATH_FILE2_NEST_YML, PATH_RESULT_NEST),
+    (PATH_FILE1_NEST_YAML, PATH_FILE2_NEST_YAML, PATH_RESULT_NEST),
 ])
 def test_generated_diff(file1, file2, result):
     string = get_answer(result)
